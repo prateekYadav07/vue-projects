b1ee3e406c3a64f1f835bdb1313c29a773e89d8f
prateekYadav07 <yadavprateek39@gmail.com>
2022-12-26 23:42:58 +0530

    components and animations

---
+++ animations/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <button @click="flag = !flag">Toggle</button>
+    <transition name="fade" mode="out-in">
+      <h2 v-if="flag" key="main">Brace for your death</h2>
+      <h2 v-else key="secondary">Oh Wrong Universe</h2>
+    </transition>
+    <transition-group name="fade" mode="out-in">
+      <ul v-for="item in Object.keys(items)" :key="item.key">
+        <li >{{item}} ->  {{items[item]}}</li>
+      </ul>    
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      flag: false,
+      items: {
+        a: 1,
+        b: 2,
+        c: 3,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.25s linear;
+}
+
+.fade-leave-from {
+  opacity: 100%;
+}
+
+.fade-leave-to {
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+</style>
