deddb8f796fcd5fd1215d3336a239901b720fd14
prateekYadav07 <yadavprateek39@gmail.com>
2022-12-28 20:19:49 +0530

    quiz app

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button @click="flag = !flag">Toggle</button>
-    <transition name="fade" mode="out-in">
+    <!-- <transition name="fade" mode="out-in">
       <h2 v-if="flag" key="main">Brace for your death</h2>
       <h2 v-else key="secondary">Oh Wrong Universe</h2>
     </transition>
@@ -9,7 +9,11 @@
       <ul v-for="item in Object.keys(items)" :key="item.key">
         <li >{{item}} ->  {{items[item]}}</li>
       </ul>    
-    </transition-group>
+    </transition-group> -->
+
+    <transition name="zoom">
+      <h2 v-if="flag">Hello</h2>
+    </transition>
   </div>
 </template>
 
@@ -39,6 +43,12 @@ export default {
   margin-top: 60px;
 }
 
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
@@ -54,5 +64,32 @@ export default {
 .fade-leave-to {
   transition: all 0.25s linear;
   opacity: 0;
+}
+
+.zoom-enter-active{
+  animation: zoom-in 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-leave-active{
+  animation: zoom-out 1s linear forwards;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
---
+++ quiz/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ctr">
+    <div class="questions-ctr">
+      <div class="progress">
+        <div class="bar"></div>
+        <div class="status">1 of 3 questions answered</div>
+      </div>
+      <div class="single-question">
+        <div class="question">Sample Question 1</div>
+        <div class="answers">
+          <div class="answer">Sample Answer 1</div>
+          <div class="answer">Sample Answer 2</div>
+          <div class="answer">Sample Answer 3</div>
+          <div class="answer">Sample Answer 4</div>
+        </div>
+      </div>
+    </div>
+    <div class="result">
+      <div class="title">You got sample result 1</div>
+      <div class="desc">
+        short description about the result
+      </div>
+    </div>
+    <button type="reset" class="reset-btn">Reset</button>
+  </div>
+</template>
+
+<script>
+ export default {
+  name: 'App'
+ }
+</script>
